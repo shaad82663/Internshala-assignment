df661e9652c48f14a6bafb387e2e8893d7fa73af
shaad82663 <60217293+shaad82663@users.noreply.github.com>
2021-12-11 12:41:43 +0530

    Add protected route functionality

--- back-end/routes/event.js
+++ back-end/routes/event.js
@@ -7,10 +7,11 @@ const { isAuthenticatedUser } = require('../middlewares/auth');
 
 
 //Base URL : /api/v1
-router.route("/event/new").post(newEvent);
-router.route("/events").get(getEvents);
-router.route("/events/today").get(getTodaysEvents);
+router.route("/event/new").post(isAuthenticatedUser, newEvent);
+router.route("/events").get(isAuthenticatedUser, getEvents);
+router.route("/events/today").get(isAuthenticatedUser, getTodaysEvents);
 router.route("/events/week").get(isAuthenticatedUser, getWeekEvents);
+
 
 
 module.exports = router;
