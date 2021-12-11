6dedfc94d66f31e9bf59555458df79b2f11dcca3
shaad82663 <60217293+shaad82663@users.noreply.github.com>
2021-12-11 12:44:40 +0530

    Edit event route

--- back-end/routes/event.js
+++ back-end/routes/event.js
@@ -8,7 +8,7 @@ const { isAuthenticatedUser } = require('../middlewares/auth');
 
 //Base URL : /api/v1
 router.route("/event/new").post(isAuthenticatedUser, newEvent);
-router.route("/events").get(isAuthenticatedUser, getEvents);
+router.route("/events").get(getEvents);
 router.route("/events/today").get(isAuthenticatedUser, getTodaysEvents);
 router.route("/events/week").get(isAuthenticatedUser, getWeekEvents);
 
