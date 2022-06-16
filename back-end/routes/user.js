2ceebb43921658dcf220b27a831c9e6822df3b77
shaad82663 <60217293+shaad82663@users.noreply.github.com>
2022-06-17 00:38:54 +0530

    Edit user file

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -5,12 +5,12 @@ const { isAuthenticatedUser } = require('../middlewares/auth');
 const { registerUser, logInUser, logout, getUsers, getUserDetails} = require('../controllers/userController');
 
 //Base URL : /api/v1
-router.route("/register").post(registerUser);
-router.route("/login").post(logInUser);
-router.route("/logout").get(logout);
+router.route("/user/register").post(registerUser);
+router.route("/user/login").post(logInUser);
+router.route("/user/logout").get(logout);
 
-router.route("/users").get(isAuthenticatedUser, getUsers);
+router.route("/user/users").get(isAuthenticatedUser, getUsers);
 
-router.route("/user-details").get(isAuthenticatedUser, getUserDetails);
+router.route("/user/user-details").get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
